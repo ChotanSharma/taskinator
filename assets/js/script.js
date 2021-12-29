65dcd3c62ab3891be799dd614a81e6aaba914f2e
loneWolf <chotansharma@yahoo.com>
2021-12-29 15:08:23 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
+var taskIdCounter = 0;
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function (event) {
   event.preventDefault();
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  if (!taskNameInput || !taskTypeInput) {
+  if (!taskNameInput === "" || !taskTypeInput === "") {
     alert("You need to fill out the task form!");
     return false;
   }
+
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").selectedIndex = 0;
+ 
+  // check if task is new or one being edited by seeing if it has a data-task-id attribute
+  var isEdit = formEl.hasAttribute("data-task-id");
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
